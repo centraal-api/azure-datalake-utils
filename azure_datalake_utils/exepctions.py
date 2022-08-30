ad9a780e407e6c60c6a4c7d1214e550f6d3adedb
German <german@centraal.studio>
2022-08-30 14:21:06 -0500

    correcion de lint formateo

--- azure_datalake_utils/exepctions.py
+++ azure_datalake_utils/exepctions.py
@@ -1,13 +1,15 @@
+"""Exepciones personalizadas."""
+
 
 class ArchivoNoEncontrado(Exception):
     """Exepction cuando un archivo no es encontrado en el Datalake."""
 
-
     def __init__(self, ruta, message="La ruta no es se encuentra, verificar que el archivo(s) existan."):
+        """Constructor de la expecion."""
         self.ruta = ruta
         self.message = message
         super().__init__(self.message)
 
     def __str__(self):
+        """Imprimir el mensaje de la exepecion."""
         return f'{self.ruta} -> {self.message}'
-    

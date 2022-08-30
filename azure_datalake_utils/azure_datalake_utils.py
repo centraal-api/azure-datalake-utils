106078b4a6796370f8867061cb89b97a2b8a8dde
German <german@centraal.studio>
2022-08-30 14:21:06 -0500

    correcion de lint formateo

--- azure_datalake_utils/azure_datalake_utils.py
+++ azure_datalake_utils/azure_datalake_utils.py
@@ -1,5 +1,5 @@
 """Main module."""
-import logging
+
 import pandas as pd
 from azure.identity import InteractiveBrowserCredential
 
@@ -7,6 +7,7 @@ from azure_datalake_utils.exepctions import ArchivoNoEncontrado
 
 
 class Datalake(object):
+    """Clase para representar operaciones de Datalake."""
 
     def __init__(self, datalake_name: str, tenant_id: str) -> None:
         """Clase para interactuar con Azure Dalake.
@@ -20,30 +21,29 @@ class Datalake(object):
         """
         self.datalake_name = datalake_name
         self.tenant_id = tenant_id
-        credentials  = InteractiveBrowserCredential(tenant_id=self.tenant_id)
+        credentials = InteractiveBrowserCredential(tenant_id=self.tenant_id)
         credentials.authenticate()
         self._credentials = credentials
-        self.storage_options =  {'account_name' : self.datalake_name, 'anon': False}
+        self.storage_options = {'account_name': self.datalake_name, 'anon': False}
 
-    
-    def read_csv(self, ruta:str, **kwargs) -> pd.DataFrame:
+    def read_csv(self, ruta: str, **kwargs) -> pd.DataFrame:
         """Leer un archivo CSV desde la cuenta de datalake.
+
+        # noqa: E501
         Esta función hace una envoltura de [pandas.read_csv](https://pandas.pydata.org/docs/reference/api/pandas.read_csv.html).
-        
+        Por favor usar la documentación de la función para determinar parametros adicionales.
         Args:
             ruta: Ruta a leeder el archivo, debe contener una referencia a un archivo
                 `.csv` o `.txt`. Recordar que la ruta debe contener esta estructura:
                 `{NOMBRE_CONTENEDOR}/{RUTA}/{nombre o patron}.csv`.
             **kwargs: argumentos a pasar a pd.read_csv.
-        
-        Returns:
 
+        Returns:
             Dataframe con la informacion del la ruta.
         """
         try:
             df = pd.read_csv(f"az://{ruta}", storage_options=self.storage_options, **kwargs)
-        except IndexError as error:
+        except IndexError:
             raise ArchivoNoEncontrado(ruta)
 
         return df
-
--- tests/test_azure_exepctions.py
+++ tests/test_azure_exepctions.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python
 """Tests for `azure_datalake_utils.exepctions` package."""
 
-import pytest
 from azure_datalake_utils.exepctions import ArchivoNoEncontrado
-
 
 
 def test_ArchivoNoEncontrado():
     """Test para ArchivoNoEncontrado."""
-
     e = ArchivoNoEncontrado("ruta")
     assert str(e) == "ruta -> La ruta no es se encuentra, verificar que el archivo(s) existan."

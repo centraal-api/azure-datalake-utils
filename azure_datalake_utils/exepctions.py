e05d16b60c84f2af18d5974feceb560783d290f2
German <german@centraal.studio>
2022-08-31 11:23:10 -0500

    completar los dos helpers más importantes

--- azure_datalake_utils/exepctions.py
+++ azure_datalake_utils/exepctions.py
@@ -13,3 +13,17 @@ class ArchivoNoEncontrado(Exception):
     def __str__(self):
         """Imprimir el mensaje de la exepecion."""
         return f'{self.ruta} -> {self.message}'
+
+
+class ExtensionIncorrecta(Exception):
+    """Exepction cuando la ruta tiene extension incorrecta."""
+
+    def __init__(self, ruta, message="La ruta no termina en la extension correcta."):
+        """Constructor de la expecion."""
+        self.ruta = ruta
+        self.message = message
+        super().__init__(self.message)
+
+    def __str__(self):
+        """Imprimir el mensaje de la exepecion."""
+        return f'{self.ruta} -> {self.message}'

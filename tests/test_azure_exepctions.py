2014ea042c8405badff126aec92a776e4ceada57
German <german@centraal.studio>
2022-08-31 11:23:10 -0500

    completar los dos helpers más importantes

--- tests/test_azure_exepctions.py
+++ tests/test_azure_exepctions.py
@@ -1,10 +1,16 @@
 #!/usr/bin/env python
 """Tests for `azure_datalake_utils.exepctions` package."""
 
-from azure_datalake_utils.exepctions import ArchivoNoEncontrado
+from azure_datalake_utils.exepctions import ArchivoNoEncontrado, ExtensionIncorrecta
 
 
 def test_ArchivoNoEncontrado():
     """Test para ArchivoNoEncontrado."""
     e = ArchivoNoEncontrado("ruta")
     assert str(e) == "ruta -> La ruta no es se encuentra, verificar que el archivo(s) existan."
+
+
+def test_ExtensionIncorrecta():
+    """Test para ArchivoNoEncontrado."""
+    e = ExtensionIncorrecta("ruta")
+    assert str(e) == "ruta -> La ruta no termina en la extension correcta."

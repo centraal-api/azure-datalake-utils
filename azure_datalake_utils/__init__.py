fc2944dcce04c9d32d84e3fc491b7f61c98e8bef
German <german@centraal.studio>
2022-09-05 20:14:55 -0500

    Bump version: 0.3.0 → 0.3.1

--- azure_datalake_utils/__init__.py
+++ azure_datalake_utils/__init__.py
@@ -7,6 +7,6 @@ Modulos exportados por este paquete.
 
 __author__ = """centraal.studio"""
 __email__ = 'equipo@centraal.studio'
-__version__ = '0.3.0'
+__version__ = '0.3.1'
 
 from azure_datalake_utils.azure_datalake_utils import Datalake
